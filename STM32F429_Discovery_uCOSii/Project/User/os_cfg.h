6439e0b579a5a4f9184595d7464e8a48c0751018
utopia <lanniaoershi@gmail.com>
2015-09-10 18:02:00 +0800

    Fix ucos build failed
    More detail refer to guide

--- STM32F429_Discovery_uCOSii/Project/User/os_cfg.h
+++ STM32F429_Discovery_uCOSii/Project/User/os_cfg.h
@@ -27,7 +27,7 @@
 
 
                                        /* ---------------------- MISCELLANEOUS ----------------------- */
-#define OS_APP_HOOKS_EN           1u   /* Application-defined hooks are called from the uC/OS-II hooks */
+#define OS_APP_HOOKS_EN           0u   /* Application-defined hooks are called from the uC/OS-II hooks */
 #define OS_ARG_CHK_EN             0u   /* Enable (1) or Disable (0) argument checking                  */
 #define OS_CPU_HOOKS_EN           1u   /* uC/OS-II hooks are found in the processor port files         */
 
